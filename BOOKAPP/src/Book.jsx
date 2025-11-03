705051b7b0d9724fce50139198c4162c34506cf6
AnuragMishra88 <anurag.mishraa1210@gmail.com>
2025-11-03 13:03:53 +0530

    BOOKAPP

--- BOOKAPP/src/Book.jsx
+++ BOOKAPP/src/Book.jsx
@@ -4,9 +4,12 @@ import './Book.css'
 function Book(){
   return(
     <div id="book">
-      <img src="" alt="" />
+      <img src="" alt="" height={100} width={100}/>
       <h1>TITLE:MAths</h1>
       <h1>250</h1>
+      <button>+</button>
+      <button>-</button>
     </div>
   )
 }
+export default Book;
